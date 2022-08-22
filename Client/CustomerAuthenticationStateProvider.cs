60a9a843acd60fc23101cc0d63873711948595eb
Sean Woodward <swoodward@intertech.com>
2022-08-22 17:29:19 -0500

    Add authentication and authorization.

---
+++ Client/CustomerAuthenticationStateProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Extensions;
+using Shared.Models;
+using System.Net.Http.Json;
+using System.Security.Claims;
+
+namespace Client
+{
+    public class CustomerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerAuthenticationStateProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var claimsIdentity = new ClaimsIdentity();
+
+            var response = await _httpClient.GetFromJsonAsync<ClientPrincipalPayLoad>("/.auth/me");
+
+            if (response is not null)
+            {
+                if (response.ClientPrincipal is not null)
+                {
+                    claimsIdentity = response.ClientPrincipal.ToClaimsIdentity();
+                }
+            }
+
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var authenticationState = new AuthenticationState(claimsPrincipal);
+
+            return authenticationState;
+        }
+
+        private class ClientPrincipalPayLoad
+        {
+            public ClientPrincipal? ClientPrincipal { get; set; }
+        }
+    }
+}
---
+++ Shared/Extensions/ClientPrincipalExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Shared.Models;
+using System.Security.Claims;
+
+namespace Shared.Extensions
+{
+    public static class ClientPrincipalExtensions
+    {
+        public static ClaimsIdentity ToClaimsIdentity(this ClientPrincipal clientPrincipal)
+        {
+            var claimsIdentity = new ClaimsIdentity(clientPrincipal.IdentityProvider);
+
+            claimsIdentity = new ClaimsIdentity(clientPrincipal.IdentityProvider);
+
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, clientPrincipal.UserDetails));
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, clientPrincipal.UserId));
+
+            claimsIdentity.AddClaims(clientPrincipal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            foreach (var claim in clientPrincipal.Claims)
+            {
+                claimsIdentity.AddClaim(new Claim(claim.Typ, claim.Val));
+            }
+
+            return claimsIdentity;
+        }
+    }
+}

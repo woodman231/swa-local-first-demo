ef097180a40f6d677480278b27bc4cb7cae3d7f5
Sean Woodward <swoodward@intertech.com>
2022-08-26 09:22:12 -0500

    revert claims to be read from x-ms-client-principal header

--- Api/Me.cs
+++ Api/Me.cs
@@ -1,36 +1,27 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Shared.Models;
-using Shared.Extensions;
+using System;
 using System.Text;
-using System.Linq;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Net;
+using System.Text.Json;
 
 namespace Api
 {
     public static class Me
     {
         [FunctionName("Me")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             var myResponse = new ClientPrincipalResponse();
 
-            var myClientPrincipal = await GetClientPrincipal(req, log);
+            var myClientPrincipal = GetClientPrincipal(req, log);
 
-            if(myClientPrincipal.UserId is not null)
+            if (myClientPrincipal.UserId is not null)
             {
                 myResponse.ClientPrincipal = myClientPrincipal;
             }
@@ -38,55 +29,24 @@ namespace Api
             return new OkObjectResult(myResponse);
         }
 
-        private static async Task<ClientPrincipal> GetClientPrincipal(HttpRequest req, ILogger log)
+        private static ClientPrincipal GetClientPrincipal(HttpRequest req, ILogger log)
         {            
-            var principal = new ClientPrincipal();            
+            var clientPrincipal = new ClientPrincipal();
 
-            var swaCookie = req.Cookies["StaticWebAppsAuthCookie"];
-
-            if(swaCookie != null)
+            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
             {
-                try
-                {
-                    string endPoint = $"{req.Scheme}://{req.Host}/.auth/me";
-
-                    HttpClient client = new HttpClient();
-                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endPoint);
-
-                    request.Headers.Add("Cookie", $"StaticWebAppsAuthCookie={swaCookie}");
-
-                    log.LogInformation($"Sending Http Request to {endPoint} with StaticWebAppsAuthCookie={swaCookie}");
-
-                    var response = await client.SendAsync(request);
-                    var information = await response.Content.ReadAsStringAsync();
-
-                    log.LogInformation($"Recieved the following response from {endPoint}: {information}");
-
-                    var jsonResult = System.Text.Json.JsonSerializer.Deserialize<ClientPrincipalPayLoad>(information, new System.Text.Json.JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    });
-
-                    return jsonResult.ClientPrincipal;
-
-                }
-                catch(Exception ex)
-                {
-                    log.LogError(ex.Message);
-                }
+                var data = header[0];
+                var decoded = Convert.FromBase64String(data);
+                var json = Encoding.UTF8.GetString(decoded);
+                clientPrincipal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
 
-            return principal;
+            return clientPrincipal;
         }
 
         private class ClientPrincipalResponse
         {
             public ClientPrincipal ClientPrincipal { get; set; } = null;
-        }
-
-        private class ClientPrincipalPayLoad
-        {
-            public ClientPrincipal? ClientPrincipal { get; set; }
         }
     }
 }
